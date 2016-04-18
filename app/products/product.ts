3d36756255f64a761564405dcd2ada0eced14d8f
gabrielgomez522 <gabriel.gomez522@outlook.com>
2016-04-18 12:17:37 -0400

    adding angular files

---
+++ app/products/product.ts
@@ -0,0 +1,11 @@
+export interface IProduct{
+    productId: number;
+    productName: string;
+    productCode: string;
+    productDate: string;
+    releaseDate: string;
+    price: number;
+    description: string;
+    starRating: number;
+    imageUrl: string;
+}
